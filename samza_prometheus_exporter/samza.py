6873d2b6cb9d1adec03dff2ecb36de1e4dabcd9b
juris <juris@zee.lv>
2016-07-15 11:15:40 +0300

    more custom metrics

--- samza_prometheus_exporter/samza.py
+++ samza_prometheus_exporter/samza.py
@@ -278,6 +278,7 @@ metrics = {
     },
     'org.apache.samza.system.chooser.BootstrappingChooserMetrics': {
         'batch-resets',
-	'lagging-batch-streams'
+	    'lagging-batch-streams',
+        'kafka-mqtt-presence-lagging-partitions'
     }
 }
